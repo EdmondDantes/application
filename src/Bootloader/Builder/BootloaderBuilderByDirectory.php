c0fca3c6448c29c34ef83081c24c2e417bd36e56
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-08-05 17:26:55 +0300

    % refactoring bootloaders

--- src/Bootloader/Builder/BootloaderBuilderByDirectory.php
+++ src/Bootloader/Builder/BootloaderBuilderByDirectory.php
@@ -5,21 +5,72 @@ namespace IfCastle\Application\Bootloader\Builder;
 
 final class BootloaderBuilderByDirectory extends BootloaderBuilderAbstract
 {
-    public function __construct(private readonly string $bootloaderDir) {}
+    public function __construct(private readonly string $bootloaderDir, protected string $appCode) {}
     
     protected function fetchBootloaders(): iterable
     {
         foreach (glob($this->bootloaderDir.'/*.ini') as $file) {
             
-            $bootloaderConfig       = parse_ini_file($file);
+            $bootloaderConfig        = $this->convertIniToArray($file);
             
-            if(false === is_array($bootloaderConfig)) {
+            if(null === $bootloaderConfig) {
                 continue;
             }
             
-            foreach ($bootloaderConfig as $bootloaderClass) {
+            if(array_key_exists('is_active', $bootloaderConfig) === false || empty($bootloaderConfig['is_active'])) {
+                continue;
+            }
+            
+            if(array_key_exists('for_application', $bootloaderConfig)
+               && is_array($bootloaderConfig['for_application'])
+               && in_array($this->appCode, $bootloaderConfig['for_application']) === false) {
+                continue;
+            }
+            
+            if(array_key_exists('bootloader', $bootloaderConfig) === false || empty($bootloaderConfig['bootloader'])) {
+                continue;
+            }
+            
+            foreach ($bootloaderConfig['bootloader'] as $bootloaderClass) {
                 yield $bootloaderClass;
             }
         }
+    }
+    
+    protected function convertIniToArray(string $file): array|null
+    {
+        $data                       = parse_ini_file($file, true);
+        
+        if(false === is_array($data)) {
+            return null;
+        }
+        
+        $result                     = [];
+        
+        // Convert all sections with dot notation to nest arrays
+        foreach ($data as $section => $values) {
+            
+            $parts                  = explode('.', $section);
+            
+            if(count($parts) === 1) {
+                $result[$section]   = $values;
+                continue;
+            }
+            
+            $pointer                = &$result;
+            
+            foreach ($parts as $part) {
+                
+                if(array_key_exists($part, $pointer) === false) {
+                    $pointer[$part] = [];
+                }
+                
+                $pointer            = &$pointer[$part];
+            }
+            
+            $pointer                = array_merge($pointer, $values);
+        }
+        
+        return $result;
     }
 }
