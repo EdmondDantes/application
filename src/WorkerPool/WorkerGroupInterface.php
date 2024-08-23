f6d813af013f7d5aa1234bd9c8b08387574834db
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-08-23 10:24:55 +0300

    + WorkerGroup

--- src/WorkerPool/WorkerGroupInterface.php
+++ src/WorkerPool/WorkerGroupInterface.php
@@ -5,5 +5,15 @@ namespace IfCastle\Application\WorkerPool;
 
 interface WorkerGroupInterface
 {
+    public function getEntryPointClass(): string;
     
+    public function getWorkerType(): WorkerTypeEnum;
+    
+    public function getWorkerGroupId(): int;
+    
+    public function getMinWorkers(): int;
+    
+    public function getMaxWorkers(): int;
+    
+    public function getGroupName(): string;
 }
