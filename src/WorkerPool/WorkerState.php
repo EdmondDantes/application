e2b8395384281df7b49f32b5f2d243a436d7af09
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-08-23 10:27:44 +0300

    + WorkerState

---
+++ src/WorkerPool/WorkerState.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace IfCastle\Application\WorkerPool;
+
+readonly class WorkerState          implements WorkerStateInterface
+{
+    public function __construct(
+        public int  $workerId,
+        public int  $groupId,
+        public bool $shouldBeStarted,
+        public int  $pid = 0
+    ) {}
+    
+    public function getWorkerId(): int
+    {
+        return $this->workerId;
+    }
+    
+    public function getGroupId(): int
+    {
+        return $this->groupId;
+    }
+    
+    public function isShouldBeStarted(): bool
+    {
+        return $this->shouldBeStarted;
+    }
+    
+    public function getPid(): int
+    {
+        return $this->pid;
+    }
+}
