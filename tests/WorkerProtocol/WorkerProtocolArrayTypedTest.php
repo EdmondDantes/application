89d9614a7bf2002d51378dbd9809bf3ce3c4d159
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-11-17 12:18:20 +0200

    + WorkerProtocolArrayTypedTest::testResponseWithException

--- tests/WorkerProtocol/WorkerProtocolArrayTypedTest.php
+++ tests/WorkerProtocol/WorkerProtocolArrayTypedTest.php
@@ -7,6 +7,7 @@ namespace IfCastle\Application\WorkerProtocol;
 
 use IfCastle\Application\Environment\SystemEnvironment;
 use IfCastle\DI\Resolver;
+use IfCastle\TypeDefinitions\Exceptions\RemoteException;
 use IfCastle\TypeDefinitions\Value\ValueBool;
 use IfCastle\TypeDefinitions\Value\ValueNumber;
 use IfCastle\TypeDefinitions\Value\ValueString;
@@ -51,5 +52,20 @@ class WorkerProtocolArrayTypedTest extends TestCase
         $parsed                     = $workerProtocolArrayTyped->parseWorkerResponse($response);
         
         $this->assertEquals('value1', $parsed);
+    }
+    
+    public function testResponseWithException(): void
+    {
+        $systemEnvironment          = new SystemEnvironment(new Resolver(), []);
+        $workerProtocolArrayTyped   = new WorkerProtocolArrayTyped($systemEnvironment);
+        
+        $response                   = $workerProtocolArrayTyped->buildWorkerResponse(
+            new \Exception('exception'),
+        );
+        
+        $parsed                     = $workerProtocolArrayTyped->parseWorkerResponse($response);
+        
+        $this->assertInstanceOf(RemoteException::class, $parsed);
+        $this->assertEquals('exception', $parsed->getMessage());
     }
 }
